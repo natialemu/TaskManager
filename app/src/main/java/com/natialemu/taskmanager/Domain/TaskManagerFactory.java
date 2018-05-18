ad04b0a40de51a49913a34730ef907361b82f171
Nathnael Alemu <nalemu@CHIC02VQ106G8WN.sea.corp.expecn.com>
2018-05-18 09:40:31 -0700

    implemented TaskManagerFactory

--- app/src/main/java/com/natialemu/taskmanager/Domain/TaskManagerFactory.java
+++ app/src/main/java/com/natialemu/taskmanager/Domain/TaskManagerFactory.java
@@ -1,5 +1,7 @@
 package com.natialemu.taskmanager.Domain;
 
+import java.util.List;
+
 /**
  * Created by Nathnael on 4/8/2018.
  *
@@ -7,6 +9,13 @@ package com.natialemu.taskmanager.Domain;
  */
 
 public class TaskManagerFactory {
+    private Graph taskGraph;
+    private Connectivity connectivity;
+
+    public TaskManagerFactory(){
+        taskGraph = new DAG();
+        connectivity = new ConnectivityImpl(taskGraph);
+    }
     /**
      *
      * Should have the ff functionalities:
@@ -22,4 +31,37 @@ public class TaskManagerFactory {
      *
      *
      */
+
+    public boolean createDependency(Item parentItem, Item childItem){
+        return taskGraph.addEdge(parentItem,childItem);
+    }
+
+    public boolean addTask(Item item){
+        return taskGraph.addItem(item);
+    }
+
+    public boolean removeTask(Item item){
+        return taskGraph.removeItem(item);
+    }
+
+    public boolean removeDependency(Item parentItem, Item childItem){
+        return taskGraph.removeEdge(parentItem,childItem);
+    }
+
+    public List<Item> getOptimalOrderOfTasks(){
+        return null;
+
+    }
+
+    public List<Item> getIndependentSetOfTasks(){
+        return null;
+    }
+
+    public List<Item> getFirstPriorityTasks(){
+        return null;
+    }
+
+    public List<Item> getAllTasks(){
+        return null;
+    }
 }
