794fdfc407ec29494acbaee01dea2995b13ecf5d
Nathnael Alemu <nalemu@CHIC02VQ106G8WN.sea.corp.expecn.com>
2018-05-28 14:59:20 -0700

    added nav drawer

--- app/src/main/java/com/natialemu/taskmanager/View/MainActivity.java
+++ app/src/main/java/com/natialemu/taskmanager/View/MainActivity.java
@@ -1,12 +1,17 @@
 package com.natialemu.taskmanager.View;
 
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.natialemu.taskmanager.R;
@@ -17,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mPager;
 
+    private DrawerLayout mDrawerLayout;
+
     private static final int NUM_PAGES = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar appBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(appBar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        appBar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        appBar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //TODO
+            }
+        });
 
         //appBar.setLogo(R.drawable.L);
 
@@ -38,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
         mPagerAdapter.addFragment(new AllTasksFragement(), "All tasks");
 
         mPager.setAdapter(mPagerAdapter);
+
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                }
+        );
 
 
 
@@ -68,8 +97,8 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()){
-            case R.id.side_bar_menu: Toast.makeText(this,"Lauch sidebar",Toast.LENGTH_SHORT);//do something;
-            case R.id.settings_item: Toast.makeText(this,"more settings will come",Toast.LENGTH_SHORT);
+            case R.id.settings_item: Toast.makeText(this,"more settings will come",Toast.LENGTH_SHORT); return true;
+            case R.id.homeAsUp: mDrawerLayout.openDrawer(GravityCompat.START); return true;
             default: return super.onOptionsItemSelected(item);
 
         }
