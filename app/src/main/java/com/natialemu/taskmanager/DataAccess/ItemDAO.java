715585e9aed8364de74291b5f9f55ef907587bdb
Nathnael Alemu <nalemu.luc.edu>
2018-07-10 14:46:21 -0700

    completed itemDAO

--- app/src/main/java/com/natialemu/taskmanager/DataAccess/ItemDAO.java
+++ app/src/main/java/com/natialemu/taskmanager/DataAccess/ItemDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class ItemDAO {
 
@@ -15,7 +16,14 @@ public class ItemDAO {
         try{
             Statement insertSatement = connection.createStatement();
 
-            String insertQuery = "INSERT INTO order_confirmation (confirmation_id,header,body) VALUES("+orderConfirmation.getConfirmationID()+", '"+orderConfirmation.getMessageHeader()+"', '"+orderConfirmation.getMessageBody()+"')";
+            String insertQuery = "INSERT INTO item_desc (item_id,item_category,priority,title," +
+                    "tag,is_recurring_task,anticipated_completion_date,item_creation_date,note)" +
+                    " VALUES("+item.getItemID()+", '" +
+                    ""+item.getCategory()+"', "+item.getPriority()+", '" +
+                    item.getTitle()+"', '" +
+                    item.getTag()+"', " + item.isReccuringTask()+ ", " +
+                    item.getAnticipatedCompletionDate() + ", " + item.getCreationDate() + ", '" +
+                    item.getNotes() + "')";
             insertSatement.executeUpdate(insertQuery);
 
             inserted = true;
@@ -36,15 +44,19 @@ public class ItemDAO {
 
     }
 
-    public boolean updateItem(Item item){
+    public boolean updateItem(Item item, int item_id){
         Connection connection = DBConnect.getDatabaseConnection();
         boolean updated = false;
 
         try{
             Statement insertSatement = connection.createStatement();
 
-            String updateQuery = "UPDATE placed_order SET order_status='" + retrievedOrder.getStringOrderState() + "" +
-                    "' WHERE order_confirmation_id=" + confirmation_id;
+            String updateQuery = "UPDATE item_desc SET priority=" + item.getPriority() + ", " +
+                    "title='"+item.getTitle() + "', item_category='"+item.getCategory() + "', " +
+                    "tag='"+item.getTag() +"', is_recurring_task=" + item.isReccuringTask() + ", " +
+                    "anticipated_completion_date=" + item.getAnticipatedCompletionDate() + ", "+
+                    "item_creation_date=" + item.getCreationDate() + ", note='"+item.getNotes()+"'," +
+                    " WHERE item_id=" + item_id;
             insertSatement.executeUpdate(updateQuery);
 
             updated = true;
@@ -66,18 +78,38 @@ public class ItemDAO {
 
     }
 
-    public Item getItem(Item item){
+    public Item getItem(int item_id){
+        int priority = 0;
+        String title = "";
+        String category = "";
+        String tag = "";
+        boolean isReccuringTask = false;
+        Date anticipatedCreationDate = null;
+        Date creationDate = null;
+        String notes = null;
+
+
+
 
         Connection connection = DBConnect.getDatabaseConnection();
         try {
             Statement selectStatement = connection.createStatement();
 
-            String selectQuery = "SELECT * from order_confirmation where confirmation_id=" + confirmationID;
+            String selectQuery = "SELECT * from item_desc where item_id=" + item_id;
             ResultSet resultSet = selectStatement.executeQuery(selectQuery);
             resultSet.next();
 
 //            body = resultSet.getString("body");
 //            header = resultSet.getString("header");
+            priority = resultSet.getInt("priority");
+            title = resultSet.getString("title");
+            tag = resultSet.getString("tag");
+            category = resultSet.getString("item_category");
+            isReccuringTask = resultSet.getBoolean("is_reccuring_task");
+            anticipatedCreationDate = resultSet.getDate("anticipated_completion_date");
+            creationDate = resultSet.getDate("item_creation_date");
+            notes = resultSet.getString("note");
+
 
         } catch (SQLException se) {
             se.printStackTrace();
@@ -89,12 +121,25 @@ public class ItemDAO {
                 } catch (SQLException e) { /* ignored */}
             }
         }
+        Item item = new Item();
+        item.setPriority(priority);
+        item.setItemID(Integer.toString(item_id));
+        item.setAnticipatedCompletionDate(anticipatedCreationDate);
+        item.setCreationDate(creationDate);
+        item.setCategory(category);
+        item.setReccuringTask(isReccuringTask);
+        item.setTag(tag);
+        item.setTitle(title);
+        item.setNotes(notes);
+        //notes not created
 
-        return null;
+
+        return item;
 
     }
 
     public boolean deleteItem(Item item){
+        //TODO
         return false;
 
     }
