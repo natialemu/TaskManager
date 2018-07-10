8313afbd5fc82331806d2ff815abe635dee15069
Nathnael Alemu <nalemu.luc.edu>
2018-07-10 15:08:51 -0700

    modified 2 dao classes

--- app/src/main/java/com/natialemu/taskmanager/DataAccess/ItemRelationShipDAO.java
+++ app/src/main/java/com/natialemu/taskmanager/DataAccess/ItemRelationShipDAO.java
@@ -1,6 +1,36 @@
 package com.natialemu.taskmanager.DataAccess;
 
+import com.natialemu.taskmanager.Domain.Item;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class ItemRelationShipDAO {
+
+    public List<Item> getChildren(int item_id){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+        return null;
+
+    }
+
+    public List<Item> getParents(int item_id){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+        return null;
+    }
+
+    public void addEdge(int source_item_id, int destination_item_id){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+
+    }
+
+    public void deleteEdge(int source_ite_id, int destination_item_id){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+
+    }
 }
 
 /**
--- app/src/main/java/com/natialemu/taskmanager/DataAccess/ObserverDAO.java
+++ app/src/main/java/com/natialemu/taskmanager/DataAccess/ObserverDAO.java
@@ -1,5 +1,11 @@
 package com.natialemu.taskmanager.DataAccess;
 
+import com.natialemu.taskmanager.Domain.ForestObserver;
+import com.natialemu.taskmanager.Domain.GraphNode;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class ObserverDAO {
     /**
      * Will contain information about observers:
@@ -7,4 +13,18 @@ public class ObserverDAO {
      *   2. Thier id
      *   3. weather they observe recurring tasks or not
      */
+
+    public List<ForestObserver> getAllObservers(){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+
+
+    }
+
+    public List<GraphNode> geSourcesObservedBy(int observer_id){
+        Connection connection = DBConnect.getDatabaseConnection();
+
+
+
+    }
 }
